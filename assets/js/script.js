efad23e4271312b9a5e57bf93d7098100412b7ab
applepieorchard <paul.miclaus1229@gmail.com>
2024-01-19 23:48:21 +0000

    Replaced repetitive code with textIs function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 // Advanced Format plugin, using for ordinals
 dayjs.extend(window.dayjs_plugin_advancedFormat);
 
+// Global Variables
+var date = "dddd [the] Do [of] MMMM, YYYY";
+var hour = "HH:mm";
+const currentDay = $("#currentDay");
+const currentHour = $("#currentHour")
+
 // Time and Date in the header of the application
 function timeIs(format, time) {
   return dayjs(time).format(format)
 };
-
-var date = "dddd [the] Do [of] MMMM, YYYY";
-var hour = "HH:mm";
-
-$("#currentDay").text(timeIs(date));
-$("#currentTime").text(timeIs(hour));
+function textIs(element, content) {
+  return element.text(content)  
+}
+textIs(currentDay, timeIs(date));
+textIs(currentHour, timeIs(hour));

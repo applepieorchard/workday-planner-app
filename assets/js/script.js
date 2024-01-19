73e20a4df53d72a6a2f1bac0d6cec09d52a672bb
applepieorchard <paul.miclaus1229@gmail.com>
2024-01-19 23:04:41 +0000

    Replaced repetitive code with timeIs function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 // Advanced Format plugin, using for ordinals
-dayjs.extend(window.dayjs_plugin_advancedFormat)
+dayjs.extend(window.dayjs_plugin_advancedFormat);
 
 // Time and Date in the header of the application
-const currentDay = dayjs().format("dddd [the] Do [of] MMMM, YYYY");
-const currentTime = dayjs().format("HH:mm")
-$("#currentDay").text(currentDay);
-$("#currentTime").text(currentTime);
+function timeIs(format, time) {
+  return dayjs(time).format(format)
+};
+
+var date = "dddd [the] Do [of] MMMM, YYYY";
+var hour = "HH:mm";
+
+$("#currentDay").text(timeIs(date));
+$("#currentTime").text(timeIs(hour));

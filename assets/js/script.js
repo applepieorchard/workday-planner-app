41f561a25d0b9b22f45fc29380e711566e62f4e3
applepieorchard <paul.miclaus1229@gmail.com>
2024-01-19 22:13:26 +0000

    Styled time blocks into a grid with flexboxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Advanced Format plugin, using for ordinals
 dayjs.extend(window.dayjs_plugin_advancedFormat)
 
+// Time and Date in the header of the application
 const currentDay = dayjs().format("dddd [the] Do [of] MMMM, YYYY");
 const currentTime = dayjs().format("HH:mm")
 $("#currentDay").text(currentDay);

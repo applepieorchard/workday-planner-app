958dcdfce554c3f10c2a64e35a58642ec3ed0085
applepieorchard <paul.miclaus1229@gmail.com>
2024-01-20 01:05:37 +0000

    Created hourIs function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,17 @@ dayjs.extend(window.dayjs_plugin_advancedFormat);
 var date = "dddd [the] Do [of] MMMM, YYYY";
 var hour = "HH:mm";
 const currentDay = $("#currentDay");
-const currentHour = $("#currentHour")
+const currentHour = $("#currentHour");
+const hourEl = [
+  $("#nine"), $("#ten"), $("#eleven"),
+  $("#twelve"), $("#thirteen"), $("#fourteen"),
+  $("#fifteen"), $("#sixteen"), $("#seventeen")
+];
+const workHours = [
+  "9", "10", "11",
+  "12", "13", "14",
+  "15", "16", "17"
+]
 
 // Time and Date in the header of the application
 function timeIs(format, time) {
@@ -13,6 +23,11 @@ function timeIs(format, time) {
 };
 function textIs(element, content) {
   return element.text(content)  
-}
+};
 textIs(currentDay, timeIs(date));
 textIs(currentHour, timeIs(hour));
+
+// Workday Hours
+function hourIs(time, min, format, date) {
+  return dayjs(date).hour(time).minute(min).format(format)
+}
